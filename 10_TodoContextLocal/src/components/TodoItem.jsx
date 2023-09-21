33e8817c79fae52353ca4e62d8b6ccc2f28b52ed
Suyog28 <suyogvmuley@gmail.com>
2023-09-21 20:08:31 +0530

    Completed

--- 10_TodoContextLocal/src/components/TodoItem.jsx
+++ 10_TodoContextLocal/src/components/TodoItem.jsx
@@ -1,17 +1,22 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react'
 import { useTodo } from '../Context/UseTodoContext';
 
+// eslint-disable-next-line react/prop-types
 function TodoItem({ todo }) {
     const [isTodoEditable, setIsTodoEditable] = useState(false)
+    // eslint-disable-next-line react/prop-types
     const [todoMsg, setTodoMsg] = useState(todo.todo)
     const { updateTodo, deleteTodo, toggleComplete } = useTodo()
 
     const editTodo = () => {
+        // eslint-disable-next-line react/prop-types
         updateTodo(todo.id, { ...todo, todo: todoMsg })
         setIsTodoEditable(false)
     }
     const toggleCompleted = () => {
         //console.log(todo.id);
+        // eslint-disable-next-line react/prop-types
         toggleComplete(todo.id)
     }
 
@@ -23,12 +28,14 @@ function TodoItem({ todo }) {
             <input
                 type="checkbox"
                 className="cursor-pointer"
+                // eslint-disable-next-line react/prop-types
                 checked={todo.completed}
                 onChange={toggleCompleted}
             />
             <input
                 type="text"
                 className={`border outline-none w-full bg-transparent rounded-lg ${isTodoEditable ? "border-black/10 px-2" : "border-transparent"
+                    // eslint-disable-next-line react/prop-types
                     } ${todo.completed ? "line-through" : ""}`}
                 value={todoMsg}
                 onChange={(e) => setTodoMsg(e.target.value)}
@@ -38,12 +45,14 @@ function TodoItem({ todo }) {
             <button
                 className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
                 onClick={() => {
+                    // eslint-disable-next-line react/prop-types
                     if (todo.completed) return;
 
                     if (isTodoEditable) {
                         editTodo();
                     } else setIsTodoEditable((prev) => !prev);
                 }}
+                // eslint-disable-next-line react/prop-types
                 disabled={todo.completed}
             >
                 {isTodoEditable ? "📁" : "✏️"}
@@ -51,6 +60,7 @@ function TodoItem({ todo }) {
             {/* Delete Todo Button */}
             <button
                 className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
+                // eslint-disable-next-line react/prop-types
                 onClick={() => deleteTodo(todo.id)}
             >
                 ❌
