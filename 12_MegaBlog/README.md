c690eaadfa19e6e40c9636e50530ba9595cffd7f
Suyog28 <suyogvmuley@gmail.com>
2023-09-24 22:15:15 +0530

    Bega Blog Project Appwrite Setup done

---
+++ 12_MegaBlog/README.md
@@ -0,0 +1,3 @@
+# React + Vite
+
+## npm Install @reduxjs/toolkit react-redux react-router-dom appwrite @tinymce/tinymce-react html-react-parser react-hook-form
---
+++ 12_MegaBlog/src/App.jsx
@@ -0,0 +1,15 @@
+
+import { config } from 'dotenv';
+import './App.css'
+
+function App() {
+  console.log(config);
+
+  return (
+    <>
+      <h1>A Blog with App Write</h1>
+    </>
+  )
+}
+
+export default App
